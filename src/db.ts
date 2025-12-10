3b6fa9ee739a58cacf45f5ace5c0a04287aac79e
michael-255 <kodiakchaos@gmail.com>
2025-12-09 20:21:08 -0500

    db as class

--- src/db.ts
+++ src/db.ts
@@ -3,16 +3,28 @@ import dexieCloud, { type DexieCloudTable } from 'dexie-cloud-addon'
 import { type LogEntry } from './tables/logs.js'
 import { type WritingEntry } from './tables/writings.js'
 
-export const db = new Dexie('shared-app-database', { addons: [dexieCloud] }) as Dexie & {
-  logs: Table<LogEntry, number, Omit<LogEntry, 'id'>>
-  writings: DexieCloudTable<WritingEntry, 'id'>
+class AppDB extends Dexie {
+  logs!: Table<LogEntry, number, Omit<LogEntry, 'id'>>
+  writings!: DexieCloudTable<WritingEntry, 'id'>
+
+  constructor() {
+    super('SharedAppDatabase', {
+      addons: [dexieCloud],
+      cache: 'immutable', // TODO
+    })
+
+    this.version(1).stores({
+      logs: '++id, created_at',
+      writings: '@id, created_at, category, *search_terms',
+    })
+
+    this.cloud.configure({
+      unsyncedTables: ['logs'],
+      databaseUrl: import.meta.env.VITE_DB_URL,
+      tryUseServiceWorker: true,
+      requireAuth: true,
+    })
+  }
 }
 
-db.version(1).stores({
-  logs: '++id, created_at',
-  writings: '@id, created_at, category, *search_terms',
-})
-
-db.cloud.configure({
-  databaseUrl: import.meta.env.VITE_DB_URL,
-})
+export const db = new AppDB()
